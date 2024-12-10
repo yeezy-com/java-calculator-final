7d35512fdc93d6cb7fa808d6fe60dceea165a1d7
GiSeok Kang <ime.rokz@gmail.com>
2024-12-10 21:56:31 +0900

    feat: 구분자 객체 추가

---
+++ src/main/java/calculator/Separator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Separator {
+
+    private final char separator;
+
+    public Separator(char delimiter) {
+        separator = delimiter;
+    }
+
+    public char getSeparator() {
+        return separator;
+    }
+}
